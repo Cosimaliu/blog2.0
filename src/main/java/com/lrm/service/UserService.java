5670b29a037cbbc412aabd80ab72565e17916c60
sicheng0302 <initial_lsc@126.com>
2021-08-31 15:10:55 +0800

    整合redis并进行简单测试 lsc

--- src/main/java/com/lrm/service/UserService.java
+++ src/main/java/com/lrm/service/UserService.java
@@ -1,6 +1,8 @@
 package com.lrm.service;
 
 import com.lrm.po.User;
+import com.lrm.vo.UserQuery;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Created by limi on 2017/10/15.
@@ -8,4 +10,16 @@ import com.lrm.po.User;
 public interface UserService {
 
     User checkUser(String username, String password);
+
+    Object listUser(Pageable pageable, UserQuery user);
+
+    User saveUser(User user);
+
+    User updateUser(Long id, User user);
+
+    void deleteUser(Long id);
+
+    User getUser(Long id);
+
+    Object listUser(Pageable pageable);
 }
--- src/main/java/com/lrm/service/UserServiceImpl.java
+++ src/main/java/com/lrm/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.lrm.service;
 import com.lrm.dao.UserRepository;
 import com.lrm.po.User;
 import com.lrm.util.MD5Utils;
+import com.lrm.vo.UserQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,5 +21,35 @@ public class UserServiceImpl implements UserService {
     public User checkUser(String username, String password) {
         User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
+    }
+
+    @Override
+    public Object listUser(Pageable pageable, UserQuery user) {
+        return null;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public Object listUser(Pageable pageable) {
+        return null;
     }
 }
